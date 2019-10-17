cd54fa95352f9cdd585e2af693733003cdd52462
Dayton Westmark <31781136+bones62@users.noreply.github.com>
2019-10-17 11:13:46 -0700

    Added a couple of buttons and things.

--- app/src/main/java/com/IceBreaker/gogoalarm/MainActivity.java
+++ app/src/main/java/com/IceBreaker/gogoalarm/MainActivity.java
@@ -2,7 +2,12 @@ package com.IceBreaker.gogoalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button alarmButton = findViewById(R.id.Alarms);
+
+        alarmButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+
 }
